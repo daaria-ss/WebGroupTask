b81c2614d76443c3333617cf31e4ae418fd1c2c8
dashk <daaria.suv@yandex.ru>
2021-03-11 21:34:02 +0300

    tasks 11-12

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PIL import Image
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 import cv2
+import math
 
 
 SCREEN_SIZE = [800, 800]
@@ -42,14 +43,19 @@ class Example(QWidget):
         
         font = pygame.font.Font(None, 24)
         input_box = pygame.Rect(10, 10, 140, 32)
+        input_box_adress = pygame.Rect(10, 400, 140, 25)
         color_inactive = pygame.Color((150, 150, 150))
         color_active = pygame.Color((50, 50, 50))
         color = color_inactive
+        color_adress = pygame.Color((50, 50, 50))
         active = False
+        active_adress = False
         text = 'Поиск'
+        text_adress = '198096'
         flagmetka = 0
         xmetka = 0
         ymetka = 0
+        flagindex = 1
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -64,7 +70,14 @@ class Example(QWidget):
                         if event.pos[1] >= 10 and event.pos[1] <= 45:
                             text = ''
                             flagmetka = 0
-                    if event.pos[0] >= 470 and event.pos[0] <= 570:
+                    elif event.pos[0] >= 160 and event.pos[0] <= 390:
+                        if event.pos[1] >= 390 and event.pos[1] <= 440:
+                            if flagindex == 0:
+                                flagindex = 1
+                            else:
+                                if flagindex == 1:
+                                    flagindex = 0
+                    elif event.pos[0] >= 470 and event.pos[0] <= 570:
                         if event.pos[1] >= 10 and event.pos[1] <= 45:
                             geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode="
                             text1 = text.replace(' ', '')
@@ -77,25 +90,115 @@ class Example(QWidget):
                                     toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
                                 except:
                                     pass
-                                toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
-                                toponym_coodrinates = toponym["Point"]["pos"]
-                                toponym_coodrinates = toponym_coodrinates.split(' ')
-                                toponym_address = toponym_address.split(', ')
-                                x = toponym_coodrinates[0]
-                                y = toponym_coodrinates[1]
-                                xmetka = x
-                                ymetka = y
+                                try:
+                                    toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+                                    toponym_coodrinates = toponym["Point"]["pos"]
+                                    toponym_coodrinates = toponym_coodrinates.split(' ')
+                                    toponym_address = toponym_address.split(', ')
+                                    x = toponym_coodrinates[0]
+                                    y = toponym_coodrinates[1]
+                                    xmetka = x
+                                    ymetka = y
+                                    text = ''
+                                    for i in range(len(toponym_address)):
+                                        text += toponym_address[i]
+                                        text += ' '
+                                    if len(toponym_address) >= 3:
+                                        flagzoom = 2
+                                    if len(toponym_address) == 2:
+                                        flagzoom = 7
+                                    if len(toponym_address) == 1:
+                                        flagzoom = 15
+                                    if flagindex == 1:
+                                        text1 = text.replace(' ', ', ')
+                                        json_data  = {"query":text1,"limit":5,"fromBound":"CITY"}
+                                        resp = requests.post('https://www.pochta.ru/suggestions/v2/suggestion.find-addresses', json=json_data)
+                                        resp.json()
+                                        try:
+                                            text_adress = resp.json()[0]['postalCode']
+                                        except:
+                                            text_adress = ''
+                                    flagmetka = 1
+                                except:
+                                    text = 'Error'
+                                    flagmetka = 0
+                    else:
+                        if event.button == 1:
+                            try:
+                                mousex = float(event.pos[0])
+                                mousey = float(event.pos[1])
+                                mousex = float(-(325 - mousex))
+                                mousey = float((225 - mousey))
+                                adress_x = (mousex / 325) * 0.0017 + float(x)
+                                adress_y = (mousey / 225) * 0.0006 + float(y)
+                                geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode="
+                                geocoder_request += str(adress_x)
+                                geocoder_request += ','
+                                geocoder_request += str(adress_y)
+                                geocoder_request += "&format=json"
+                                response = requests.get(geocoder_request)
+                                if response:
+                                    json_response = response.json()
+                                    try:
+                                        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+                                    except:
+                                        pass
+                                    try:
+                                        toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+                                        toponym_coodrinates = toponym["Point"]["pos"]
+                                        toponym_coodrinates = toponym_coodrinates.split(' ')
+                                        toponym_address = toponym_address.split(', ')
+                                        xmetka = adress_x
+                                        ymetka = adress_y
+                                        flagmetka = 1
+                                        text = ''
+                                        for i in range(len(toponym_address)):
+                                            text += toponym_address[i]
+                                            text += ' '
+                                    except:
+                                        pass
+                            except:
+                                pass
+                        elif event.button == 3:
+                            min_rad = 99999
+                            print('Ждите около 15 секунд')
+                            for i in range(-50, 51, 20):
+                                for j in range(-50, 51, 20):
+                                    mousex = float(event.pos[0] + i)
+                                    mousey = float(event.pos[1] + j)
+                                    mousex = float(-(325 - mousex))
+                                    mousey = float((225 - mousey))
+                                    adress_x = (mousex / 325) * 0.0017 + float(x)
+                                    adress_y = (mousey / 225) * 0.0006 + float(y)
+                                    try:
+                                        geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode="
+                                        geocoder_request += str(adress_x)
+                                        geocoder_request += ','
+                                        geocoder_request += str(adress_y)
+                                        geocoder_request += "&format=json"
+                                        response = requests.get(geocoder_request)
+                                        if response:
+                                            json_response = response.json()
+                                            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+                                            toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+                                            toponym_coodrinates = toponym["Point"]["pos"]
+                                            toponym_coodrinates = toponym_coodrinates.split(' ')
+                                            toponym_address = toponym_address.split(', ')
+                                            rad = math.sqrt(i * i + j * j)
+                                            if min_rad > rad:
+                                                min_x = adress_x
+                                                min_y = adress_y
+                                                min_rad = rad
+                                    except:
+                                        pass
+                            if min_rad < 99999:
+                                xmetka = adress_x
+                                ymetka = adress_y
+                                flagmetka = 1
                                 text = ''
                                 for i in range(len(toponym_address)):
                                     text += toponym_address[i]
                                     text += ' '
-                                if len(toponym_address) >= 3:
-                                    flagzoom = 2
-                                if len(toponym_address) == 2:
-                                    flagzoom = 7
-                                if len(toponym_address) == 1:
-                                    flagzoom = 15
-                                flagmetka = 1
                 if event.type == pygame.KEYDOWN:
                     if active:
                         if event.key == pygame.K_BACKSPACE:
@@ -147,9 +250,9 @@ class Example(QWidget):
             sait += str(450)
             if flagmetka == 1:
                 sait += "&pt="
-                sait += xmetka
+                sait += str(xmetka)
                 sait += ","
-                sait += ymetka
+                sait += str(ymetka)
                 sait += ",pm2rdl"
             self.getImage(sait)
             all_sprites = pygame.sprite.Group()
@@ -171,12 +274,22 @@ class Example(QWidget):
             all_sprites.add(sprite2)
             sprite2.rect.x = 415
             sprite2.rect.y = 10
+            sprite3 = pygame.sprite.Sprite()
+            sprite3.image = load_image("Button1.png")
+            sprite3.rect = sprite3.image.get_rect()
+            all_sprites.add(sprite3)
+            sprite3.rect.x = 160
+            sprite3.rect.y = 390
             all_sprites.draw(screen)
             txt_surface = font.render(text, True, color)
+            txt_surface_adress = font.render(text_adress, True, color_adress)
             width = max(400, txt_surface.get_width()+10)
             input_box.w = width
             screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
             pygame.draw.rect(screen, color, input_box, 2)
+            if flagindex == 1:
+                screen.blit(txt_surface_adress, (input_box_adress.x+5, input_box_adress.y+5))
+                pygame.draw.rect(screen, color_adress, input_box_adress, 2)
             pygame.display.flip()
             pygame.time.delay(1)
         pygame.quit()
